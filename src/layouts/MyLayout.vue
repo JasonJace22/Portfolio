06db863f1ab2055e5a991698b6f33d8f229082e0
Jason Alexander Obernberger <jaaay22@gmx.de>
2019-03-29 22:18:34 +0100

    nav bar

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,15 +1,45 @@
 
 <template>
-  <q-layout view="hhh lpR fff">
+  <q-layout view="hhh lpr fff">
 
-    <q-header reveal elevated class="bg-primary text-black">
+    <q-header reveal elevated class="text-black">
     <q-toolbar>
-      <q-btn flat label="Homepage" />
+      <q-btn flat label="LOGO: J" />
       <q-space />
-      <!--
-        notice shrink property since we are placing it
-        as child of QToolbar
-      -->
+   <!-- home -->
+<q-btn class="underline" flat label="HOME" />
+   <!-- more -->
+<q-btn class="underline" flat label="ABOUT" />
+   <!-- more -->
+<q-btn class="underline" flat label="RESUME" />
+   <!-- more -->
+<q-btn class="underline" flat label="SKILLS" />
+   <!-- more -->
+<q-btn class="underline" flat label="KONTAKT" />
+   <!-- more -->
+          <!-- ENDE HEADER -->
+    </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal elevated class="bg-primary text-black">
+      <q-toolbar>
+        <q-space />
+   <!-- Github -->
+      <q-item clickable v-ripple @click="github('http://www.github.de')">
+      <q-item-section side class="q-pa-xs">
+        <q-avatar rounded size="25px">
+          <img src="../statics/github.png" />
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Github</q-item-label>
+      </q-item-section>
+    </q-item>
+   <!-- instagram -->
 <q-item clickable v-ripple @click="instagram('http://www.instagram.de')">
       <q-item-section side class="q-pa-xs">
         <q-avatar rounded size="25px">
@@ -20,22 +50,6 @@
         <q-item-label>Instagram</q-item-label>
       </q-item-section>
     </q-item>
-          <!-- ENDE HEADER -->
-    </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-    <q-footer reveal elevated class="bg-grey-8 text-black">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar-framework.org/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
       </q-toolbar>
     </q-footer>
 
@@ -49,11 +63,15 @@ export default {
   name: 'MyLayout',
   data () {
     return {
+      scrollPos: false
     }
   },
   methods: {
     openURL,
     instagram (thisURL) {
+      openURL(thisURL)
+    },
+    github (thisURL) {
       openURL(thisURL)
     }
   }
@@ -61,4 +79,28 @@ export default {
 </script>
 
 <style>
+
+.headerstyletop {
+  background-color: #FFFFFF00;
+  color: white !important;
+}
+
+.headerstyleNotTop {
+  background-color: white;
+  color: black !important;
+}
+
+.underline:after {
+  content: ''; /* content ist leer... */
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0%;
+  border-bottom: 2px solid darkcyan;
+  transition: 0.5s;
+}
+
+.underline:hover:after {
+  width: 100%;
+}
 </style>
